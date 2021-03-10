160c0157ed8c11ff9189a0776c5b16188ff20240
Jesus.Garcia <jesus.m.garcia.fernandez@gmail.com>
2021-03-10 02:10:21 +0100

    Added "deep" version to clone/merge functions

--- 2_Language/Exercises/playground_javascript/src/exercises/cloneMerge.js
+++ 2_Language/Exercises/playground_javascript/src/exercises/cloneMerge.js
@@ -5,9 +5,18 @@ function clone(source) {
   return {...source};
 }
 
+function deepClone(source) {
+  return JSON.parse(JSON.stringify(source));
+}
+
 function merge(source, target) {
   return {...clone(target), ...clone(source)};
 }
 
+function deepMerge(source, target) {
+  return {...deepClone(target), ...deepClone(source)};
+}
+
 // El resultado de mezclar a sobre b sería:
 console.log(merge(a, b)); // {name: "Maria", age: 31, married: true, surname: "Ibañez", country: "SPA"}
+console.log(deepMerge(a, b)); // {name: "Maria", age: 31, married: true, surname: "Ibañez", country: "SPA"}
