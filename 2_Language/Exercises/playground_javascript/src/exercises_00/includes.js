a6db50bdfa5576fed813d6f34c8639425b2bc4f6
Jesus.Garcia <jesus.m.garcia.fernandez@gmail.com>
2021-03-14 07:02:20 +0100

    Changed folder structure and added exercises templates

---
+++ 2_Language/Exercises/playground_javascript/src/exercises_00/includes.js
@@ -0,0 +1,14 @@
+function includes(array, value) {
+  // Implementation here
+}
+
+// Ejemplo:
+console.log(includes([1, 2, 3], 3)); // true
+console.log(includes([1, 2, 3], 0)); // false
+
+
+// Challenge:
+//
+// El ejercicio anterior puede quedar simplificado
+// si utilizas una función de los arrays que devuelve
+// el índice de un elemento dado.
---
+++ 2_Language/Exercises/playground_javascript/src/exercises_00/primes.js
@@ -0,0 +1,24 @@
+function isPrime(num) {
+
+}
+
+function showPrimes(from, to) {
+  // Implementation here.
+}
+
+showPrimes(1, 10);
+showPrimes(1, 100);
+
+// Challenge:
+//
+// Se puede mejorar mucho el rendimiento del ejercicio anterior.
+// Al buscar si un numero es primo, podemos dejar de comprobar
+// si es divisible por cada entero mayor que 1 una vez alcancemos
+// la raiz cuadrada de dicho número.
+
+function showPrimes2(from, to) {
+
+}
+
+showPrimes2(1, 10);
+showPrimes2(1, 100);
