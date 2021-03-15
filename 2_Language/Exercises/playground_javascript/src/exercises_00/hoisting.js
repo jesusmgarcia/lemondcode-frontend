59c24c6fa1229cd51fbd8bb4939093c2e0e1dc7e
Jesus.Garcia <jesus.m.garcia.fernandez@gmail.com>
2021-03-15 09:44:41 +0100

    Added hoisting exercise

--- 2_Language/Exercises/playground_javascript/src/exercises_00/hoisting.js
+++ 2_Language/Exercises/playground_javascript/src/exercises_00/hoisting.js
@@ -1,7 +1,9 @@
+// ------------------------------------------------------------
+// ------------------------------------------------------------
 // Apartado A
 // ¿Cual crees que será el resultado de la consola y porqué?
-//
 // TIP: escribe el código equivalente.
+// ------------------------------------------------------------
 
 function f() {
   console.log(a);
@@ -14,8 +16,37 @@ function f() {
   console.log(a);
 }
 
+// Código equivalente
+
+/*
+function f() {
+  var a;
+  function g() {
+    return a;
+  }
+
+  console.log(a);
+  console.log(g());
+
+  a = "good job!";
+
+  console.log(a);
+}
+*/
+
+// Resultado: No hay resultado, no se ejecuta la función f()
+// Resultado si se ejecutara:
+// -> undefined
+// -> undefined
+// -> "good job!"
+
+
+
+// ------------------------------------------------------------
 // Apartado B
 // ¿Y ahora?
+// ------------------------------------------------------------
+
 
 var a = 1;
 
@@ -30,8 +61,43 @@ console.log(a);
 console.log(b);
 console.log(c);
 
+// Código equivalente
+
+/*
+var a;
+var b;
+
+function f() {
+  var a;
+  var c;
+
+  console.log(a);
+
+  a = 2;
+  b = 4;
+  c = 3;
+}
+
+a = 1;
+
+f();
+
+console.log(a);
+console.log(b);
+console.log(c);
+*/
+
+// Resultado:
+// -> undefined
+// -> 1
+// -> 4
+// -> error
+
+
+// ------------------------------------------------------------
 // Apartado C
 // ¿Y con esta ligera variación?
+// ------------------------------------------------------------
 
 f();
 var a = 1;
@@ -45,3 +111,31 @@ function f() {
 console.log(a);
 console.log(b);
 console.log(c);
+
+// Código equivalente
+
+/*
+var a;
+var b;
+
+function f() {
+  var c;
+
+  console.log(a);
+  b = 4;
+  c = 3;
+}
+
+f();
+a = 1;
+
+console.log(a);
+console.log(b);
+console.log(c);
+*/
+
+// Resultado:
+// -> undefined
+// -> 1
+// -> 4
+// -> error
