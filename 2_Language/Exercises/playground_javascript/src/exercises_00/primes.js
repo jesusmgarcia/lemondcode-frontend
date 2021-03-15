72ae6bb277cfc36061b18f31b38630ad26c9ef4d
Jesus.Garcia <jesus.m.garcia.fernandez@gmail.com>
2021-03-15 08:43:27 +0100

    Added primes and subsets exercises

--- 2_Language/Exercises/playground_javascript/src/exercises_00/primes.js
+++ 2_Language/Exercises/playground_javascript/src/exercises_00/primes.js
@@ -1,9 +1,22 @@
+// Número primo = número natural (1,2 ... k-1, k) divisible por 1 y por si mismo
+// Las funciones isPrime asumen un número natural
+
 function isPrime(num) {
 
+  for (let divNum = 2; divNum < num; divNum++) {
+    if (num % divNum === 0) {
+      return false;
+    }
+  }
+
+  return num !== 1;
 }
 
+
 function showPrimes(from, to) {
-  // Implementation here.
+  for(let count = from; count <= to; count++) {
+    console.log(count + (isPrime(count) ? " is PRIME!": " is not a prime"));
+  }
 }
 
 showPrimes(1, 10);
@@ -16,8 +29,23 @@ showPrimes(1, 100);
 // si es divisible por cada entero mayor que 1 una vez alcancemos
 // la raiz cuadrada de dicho número.
 
-function showPrimes2(from, to) {
+function isPrime2(num) {
 
+  for (let divNum = 2; divNum < Math.floor(Math.sqrt(num)); divNum++) {
+    if (num % divNum === 0) {
+      return false;
+    }
+  }
+
+  return num !== 1;
+}
+
+
+
+function showPrimes2(from, to) {
+  for(let count = from; count <= to; count++) {
+    console.log(count + (isPrime(count) ? " is PRIME!": " is not a prime"));
+  }
 }
 
 showPrimes2(1, 10);
--- 2_Language/Exercises/playground_javascript/src/exercises_00/subsets.js
+++ 2_Language/Exercises/playground_javascript/src/exercises_00/subsets.js
@@ -1,5 +1,13 @@
 function subsets(word) {
-  // Implementation here
+
+  let subsetArray = [];
+
+  for(let count = 1; count < word.length; count++) {
+    subsetArray.push(word.slice(count));
+  }
+
+  return subsetArray;
+
 }
 
 // Ejemplo
@@ -12,3 +20,10 @@ console.log(subsets("message")); // ["essage", "ssage", "sage", "age", "ge", "e"
 //
 // TIP: Una forma válida de "iterar" es utilizando algún método
 // de los Arrays: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/prototype
+
+function subsets2(word) {
+  return Object.keys(new String(word)).slice(1).map( element => word.slice(element));
+}
+
+// Ejemplo
+console.log(subsets2("message")); // ["essage", "ssage", "sage", "age", "ge", "e"]
