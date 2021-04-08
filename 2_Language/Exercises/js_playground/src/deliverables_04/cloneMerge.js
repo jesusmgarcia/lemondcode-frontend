cca2f65ed9c8fbf6d2e927fb8cce20557c55d222
Jesus.Garcia <jesus.m.garcia.fernandez@gmail.com>
2021-04-08 18:43:11 +0200

    Added comments of usage in cloneMerge exercise

--- 2_Language/Exercises/js_playground/src/deliverables_04/cloneMerge.js
+++ 2_Language/Exercises/js_playground/src/deliverables_04/cloneMerge.js
@@ -1,3 +1,4 @@
+// We assume objects without functions, undefined props, etc.
 var a = { name: "Maria", surname: "Ibañez", country: "SPA" };
 var b = { name: "Luisa", age: 31, married: true };
 
@@ -20,3 +21,30 @@ function deepMerge(source, target) {
 // El resultado de mezclar a sobre b sería:
 console.log(merge(a, b)); // {name: "Maria", age: 31, married: true, surname: "Ibañez", country: "SPA"}
 console.log(deepMerge(a, b)); // {name: "Maria", age: 31, married: true, surname: "Ibañez", country: "SPA"}
+
+// a and b are not modified
+console.log(a);
+console.log(b);
+
+// NOTE: this methods won't work for objects with functions, undefined, etc
+
+var c = {
+  name: "hola",
+  f: () => console.log("a"),
+  cc: undefined,
+  obj: {
+    a: 1,
+    b: "string"
+  }
+};
+
+console.log(c);
+console.log(deepClone(c));
+
+var shallowCopiedA = a;
+var clonedA = clone(a);
+var deepClonedA = deepClone(a);
+
+console.log(a === shallowCopiedA);
+console.log(a === clonedA);
+console.log(a === deepClonedA);
