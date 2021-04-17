7117ed905a41eee6b776d854d5d36b652efd954e
Jesus.Garcia <jesus.m.garcia.fernandez@gmail.com>
2021-04-17 02:30:47 +0200

    Added sass styles

--- 3_Bundlers/webpack-first/config/webpack.base.js
+++ 3_Bundlers/webpack-first/config/webpack.base.js
@@ -1,17 +1,40 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 const basePath = path.resolve(process.cwd());
 
 module.exports = {
   context: path.join(basePath, "src"),
-  entry: ["./index.js"],
+  entry: {
+    app: "./index.js",
+    appStyles: ["./styles.scss"]
+  },
+  output: {
+    filename: "[name].[chunkhash].js",
+    path: path.join(basePath, "dist"),
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass"),
+            },
+          },
+        ],
       },
     ],
   },
@@ -23,6 +46,11 @@ module.exports = {
       filename: "index.html", //Name of file in ./dist/
       template: "index.html", //Name of template in ./src
       hash: true,
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
     }),
   ]
 };
