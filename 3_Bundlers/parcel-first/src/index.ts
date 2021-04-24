72d139be1d87cf8a5a153b3f94d8ec09689736c1
Jesus.Garcia <jesus.m.garcia.fernandez@gmail.com>
2021-04-24 14:43:34 +0200

    Added parcel w scss, image loading and ts configs

---
+++ 3_Bundlers/parcel-first/src/index.ts
@@ -0,0 +1,10 @@
+import "./scss/styles.scss";
+
+const sampleNumber = 1;
+console.log(`Hello from sample ${sampleNumber}`);
+
+
+const numberA: number = 2;
+const numberB: number = 3;
+
+console.log(numberA + numberB);
