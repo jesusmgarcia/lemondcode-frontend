3ab84b8f8d067ac78e9f413eaf12cd2762731467
Jesus.Garcia <jesus.m.garcia.fernandez@gmail.com>
2021-06-18 12:04:31 +0200

    Added material-ui to detail page

--- 4_Frameworks/React/basico/src/pages/detail/detail.tsx
+++ 4_Frameworks/React/basico/src/pages/detail/detail.tsx
@@ -1,43 +1,80 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import * as classes from "./detail.styles";
 
+const useStyles = makeStyles({
+	root: {
+		maxWidth: 345
+	},
+	media: {
+		height: 140
+	}
+});
 interface MemberDetailEntity {
-  id: string;
-  login: string;
-  name: string;
-  company: string;
-  bio: string;
+	id: string;
+	login: string;
+	avatar_url: string;
+	name: string;
+	company: string;
+	bio: string;
 }
 
 const createDefaultMemberDetail = () => ({
-  id: "",
-  login: "",
-  name: "",
-  company: "",
-  bio: "",
+	id: "",
+	login: "",
+	avatar_url: "",
+	name: "",
+	company: "",
+	bio: ""
 });
 
 export const DetailPage: React.FC = () => {
-  const [member, setMember] = React.useState<MemberDetailEntity>(
-    createDefaultMemberDetail()
-  );
-  const { id } = useParams();
+	const [member, setMember] = React.useState<MemberDetailEntity>(
+		createDefaultMemberDetail()
+	);
+	const { id } = useParams();
+	const cardClasses = useStyles();
 
-  React.useEffect(() => {
-    fetch(`https://api.github.com/users/${id}`)
-      .then((response) => response.json())
-      .then((json) => setMember(json));
-  }, []);
+	React.useEffect(() => {
+		fetch(`https://api.github.com/users/${id}`)
+			.then((response) => response.json())
+			.then((json) => setMember(json));
+	}, []);
 
-  return (
-    <>
-      <h2>Hello from Detail page</h2>
-      <p> id: {member.id}</p>
-      <p> login: {member.login}</p>
-      <p> name: {member.name}</p>
-      <p> company: {member.company}</p>
-      <p> bio: {member.bio}</p>
-      <Link to="/list">Back to list page</Link>
-    </>
-  );
+	return (
+		<div className={classes.root}>
+			<Card className={cardClasses.root}>
+				<CardActionArea>
+					<CardMedia
+						className={cardClasses.media}
+						image={member.avatar_url}
+						title="Contemplative Reptile"
+					/>
+					<CardContent>
+						<Typography gutterBottom variant="h5" component="h2">
+							{member.name}
+						</Typography>
+						<Typography variant="body2" color="textSecondary" component="p">
+							{member.bio}
+						</Typography>
+					</CardContent>
+				</CardActionArea>
+				<CardActions>
+					<Link to="/list">
+						<Button size="small" color="primary">
+							Back to list page
+						</Button>
+					</Link>
+				</CardActions>
+			</Card>
+		</div>
+	);
 };
