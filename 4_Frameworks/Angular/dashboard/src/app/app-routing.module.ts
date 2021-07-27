f5adca5baecd2fa2ebf5e77359433de1a6fa476b
Jesus.Garcia <jesus.m.garcia.fernandez@gmail.com>
2021-07-27 22:21:51 +0200

    Added Material and footer, menus, about, crud, dashboard, gallery, home , login and profile components

--- 4_Frameworks/Angular/dashboard/src/app/app-routing.module.ts
+++ 4_Frameworks/Angular/dashboard/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { CrudComponent } from './pages/crud/crud.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'crud', component: CrudComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'profile', component: ProfileComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- 4_Frameworks/Angular/dashboard/src/app/app.module.ts
+++ 4_Frameworks/Angular/dashboard/src/app/app.module.ts
@@ -4,17 +4,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PrivateMenuComponent } from './layout/private-menu/private-menu.component';
+import { PublicMenuComponent } from './layout/public-menu/public-menu.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AboutComponent } from './pages/about/about.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { CrudComponent } from './pages/crud/crud.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { MaterialModule } from './material-module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrivateMenuComponent,
+    PublicMenuComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    AboutComponent,
+    DashboardComponent,
+    GalleryComponent,
+    CrudComponent,
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
