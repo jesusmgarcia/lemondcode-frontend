edffc5036a04546617f181b74e694f16d2d2e0e2
Jesus.Garcia <jesus.m.garcia.fernandez@gmail.com>
2021-08-23 21:25:33 +0200

    Added jest configuration for module alias

--- 5_Testing/tests/config/test/jest.js
+++ 5_Testing/tests/config/test/jest.js
@@ -4,4 +4,14 @@ module.exports = {
   restoreMocks: true,
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/config/test/setup-after.ts'],
+  moduleNameMapper: {
+    "^common/(.*)": "<rootDir>/src/common/$1",
+    "^common-app/(.*)": "<rootDir>/src/common-app/$1",
+    "^core/(.*)": "<rootDir>/src/core/$1",
+    "^layouts/(.*)": "<rootDir>/src/layouts/$1",
+    "^pods/(.*)": "<rootDir>/src/pods/$1",
+    "^scenes/(.*)": "<rootDir>/src/scenes/$1",
+
+
+  }
 };
