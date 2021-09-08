7ae9a238c9d08a16a9b074263d5a38ded1157542
Jesus.Garcia <jesus.m.garcia.fernandez@gmail.com>
2021-09-08 20:48:50 +0200

    Fixed jest configuration

--- 5_Testing/tests/config/test/jest.js
+++ 5_Testing/tests/config/test/jest.js
@@ -1,6 +1,6 @@
 module.exports = {
   rootDir: '../../',
-  modulePathIgnorePatterns: ["<rootDir>/cypress/"],
+  testPathIgnorePatterns: ["<rootDir>/cypress/"],
   preset: 'ts-jest',
   restoreMocks: true,
   testEnvironment: 'jsdom',
